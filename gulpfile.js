e45b37d262970b20ef6201dd4f50e0fe7166ecad
Sienna <sienna.m.wood@gmail.com>
2016-06-21 16:03:05 -0600

    implement nunjucks templates and rendering, reorganize directories

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Sienna on 6/21/16.
+ */
+
+var gulp = require('gulp');
+
+//////////////////////
+/* Nunjucks Scripts */
+//////////////////////
+var nunjucksRender = require('gulp-nunjucks-render');
+gulp.task('nunjucks', function() {
+    // Gets .html and .nunjucks files in pages
+    return gulp.src('site/content/**/*.+(html|nunjucks)')
+    // Renders template with nunjucks
+        .pipe(nunjucksRender({
+            path: ['site/templates']
+        }))
+        // output files in app folder
+        .pipe(gulp.dest('site'))
+});
+
+
