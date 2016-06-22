4e0a0893eec3daa31ca47e7d2fcea797965ae93b
Sienna <sienna.m.wood@gmail.com>
2016-06-21 18:09:10 -0600

    link fixes

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,7 @@ var gulp = require('gulp');
 var nunjucksRender = require('gulp-nunjucks-render');
 gulp.task('nunjucks', function() {
     // Gets .html and .nunjucks files in pages
-    return gulp.src('site/content/**/*.+(html|nunjucks)')
+    return gulp.src('site/content/**/*.+(html|shtml|nunjucks)')
     // Renders template with nunjucks
         .pipe(nunjucksRender({
             path: ['site/templates']
