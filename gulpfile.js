3dd02d05be5b67f9d0c5537ea2daf47f19a3a716
Sienna <sienna.m.wood@gmail.com>
2019-04-27 08:46:14 -0600

    add template and content watcher, fix indentation

--- gulpfile.js
+++ gulpfile.js
@@ -19,85 +19,85 @@ gulp.task('default', ['build']);
 /* Nunjucks
  * -----------------------------------*/
 gulp.task('nunjucks', function () {
-    // Gets .html, .shtml, and .nunjucks files in sources/content
-    return gulp.src('./sources/content/**/*.+(html|shtml|nunjucks)')
-    // Renders template with nunjucks
-        .pipe(nunjucksRender({
-            path: ['./sources/templates']
-        }))
-        // output files in build folder
-        .pipe(gulp.dest('./build')
+  // Gets .html, .shtml, and .nunjucks files in sources/content
+  return gulp.src('./sources/content/**/*.+(html|shtml|nunjucks)')
+  // Renders template with nunjucks
+    .pipe(nunjucksRender({
+      path: ['./sources/templates'],
+    }))
+    // output files in build folder
+    .pipe(gulp.dest('./build')
     );
 });
 
 /* Change extension on 404 to .shtml
  * -----------------------------------*/
 gulp.task('ext404', ['nunjucks'], function () {
-    gulp.src('./build/404.html')
-        .pipe(ext_replace('.shtml'))
-        .pipe(gulp.dest('./build'));
-    return del([
-        './build/404.html'
-    ]);
+  gulp.src('./build/404.html')
+    .pipe(ext_replace('.shtml'))
+    .pipe(gulp.dest('./build'));
+  return del([
+    './build/404.html',
+  ]);
 });
 
 /* Copy Font-Awesome Fonts to build
  * -----------------------------------*/
 gulp.task('copyFonts', function () {
-    gulp.src('./node_modules/font-awesome/fonts/*')
-        .pipe(gulp.dest('./build/fonts')
+  gulp.src('./node_modules/font-awesome/fonts/*')
+    .pipe(gulp.dest('./build/fonts')
     );
 });
 
 /* Compile LESS to sources
  * -----------------------------------*/
 gulp.task('less', function () {
-    return gulp.src('./sources/css/*.+(less|css)')
-        .pipe(less({
-            paths: [gpath.join(__dirname, 'less', 'includes')]
-        }))
-        .pipe(gulp.dest('./sources/css')
+  return gulp.src('./sources/css/*.+(less|css)')
+    .pipe(less({
+      paths: [gpath.join(__dirname, 'less', 'includes')],
+    }))
+    .pipe(gulp.dest('./sources/css')
     );
 });
 
 /* Copy CSS to build
  * -----------------------------------*/
 gulp.task('copyCSS', ['less'], function () {
-    gulp.src('./sources/css/mcc_styles.css')
-        .pipe(gulp.dest('./build/css')
+  gulp.src('./sources/css/mcc_styles.css')
+    .pipe(gulp.dest('./build/css')
     );
 });
 
 /* Copy JS to build
  * -----------------------------------*/
 gulp.task('copyJS', function () {
-    gulp.src(['./sources/js/mcc_scripts.js',
-        './sources/sienna-boilerplate/sienna-boilerplate.js'])
-        .pipe(gulp.dest('./build/js')
+  gulp.src(['./sources/js/mcc_scripts.js',
+    './sources/sienna-boilerplate/sienna-boilerplate.js'])
+    .pipe(gulp.dest('./build/js')
     );
 });
 
 /* Copy PHP to build
  * -----------------------------------*/
 gulp.task('copyPHP', function () {
-    gulp.src('./sources/php/mail.php')
-        .pipe(gulp.dest('./build/php')
-        );
+  gulp.src('./sources/php/mail.php')
+    .pipe(gulp.dest('./build/php')
+    );
 });
 
 /* Copy images to build
  * -----------------------------------*/
 gulp.task('copyImages', ['less'], function () {
-    gulp.src('./sources/images/**/*')
-        .pipe(gulp.dest('./build/images')
+  gulp.src('./sources/images/**/*')
+    .pipe(gulp.dest('./build/images')
     );
 });
 
 /* Copy scores to build
  * -----------------------------------*/
 gulp.task('copyScores', function () {
-    gulp.src('./sources/scores/**/*')
-        .pipe(gulp.dest('./build/scores')
+  gulp.src('./sources/scores/**/*')
+    .pipe(gulp.dest('./build/scores')
     );
 });
 
@@ -107,16 +107,22 @@ gulp.task('watchStyles', function () {
   gulp.watch('sources/css/mcc_styles.less', ['copyCSS']);
 });
 
+/* Watch content and templates
+ * ---------------------------------- */
+gulp.task('watchTemplates', function () {
+  gulp.watch(['sources/content/**/*', 'templates/**/*'], ['nunjucks']);
+});
+
 /* Local Server
  * ---------------------------------- */
-gulp.task('serve', ['watchStyles', 'build'], function () {
-    gulp.src('build')
-        .pipe(webserver({
-            port: '9090',
-            livereload: true,
-            fallback: 'build/index.html',
-            open: true
-        })
+gulp.task('serve', ['watchStyles', 'watchTemplates', 'build'], function () {
+  gulp.src('build')
+    .pipe(webserver({
+        port: '9090',
+        livereload: true,
+        fallback: 'build/index.html',
+        open: true,
+      })
     );
 });
 
